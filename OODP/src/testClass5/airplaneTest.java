c4999eb7fddcd50cf335a8c39d114820709bbbb5
jmorri13 <jmorri13@cspc055.dcs.bbk.ac.uk>
2013-02-07 20:42:37 +0000

    Change-Id: Ibc40256127587698187dd45222b7c2f3671ac71d Signed-off-by: jmorri13 <jmorri13@cspc055.dcs.bbk.ac.uk>

---
+++ OODP/src/testClass5/airplaneTest.java
@@ -0,0 +1,16 @@
+package testClass5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class airplaneTest {
+	
+	@Test 
+	public void test(){
+		fail("Not yet doing anything");
+	}
+	
+
+}
