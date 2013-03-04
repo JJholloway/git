fa82476ac1969fdcaf52ac92d03f0f021d4eab6f
jmorri13 <joeymorrison358@hotmail.com>
2013-03-04 20:33:53 +0000

    First test class built

---
+++ groupWork/src/TestPoker/TestCardHandler.java
@@ -0,0 +1,29 @@
+package TestPoker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Poker.PlayHandler;
+import Poker.Request;
+import Poker.cardRequestHandler;
+
+
+
+public class TestCardHandler {
+
+	@Test
+	public void test() {
+		PlayHandler classUnderTest = new cardRequestHandler();
+		
+		Request request = new Request ("cardRequest", 5);
+		
+		Boolean actualOutput = classUnderTest.canHandle(request);
+		
+		assertTrue("Handler failed to grab", actualOutput);
+		
+	}
+
+}
+
+
