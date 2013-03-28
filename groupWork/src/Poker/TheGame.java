d63a836503b0233a9284fafd443b7703e0c900b6
Tiago Dall Igna <tiagodll@gmail.com>
2013-03-28 15:12:28 +0100

    Just tiding the deck

--- groupWork/src/Poker/TheGame.java
+++ groupWork/src/Poker/TheGame.java
@@ -6,11 +6,8 @@ public class TheGame
 { 
 	public static void main(String [] args)
 	{
-		List<iCard> deck = CardFactory.getDeck();
+		List<iCard> deck = Deck.getInstance().getRequestedCards(5);
 		for(int i=0; i<deck.size(); i++)
 			System.out.println(deck.get(i).toString());
-		
-	
-		
 	}
 }
